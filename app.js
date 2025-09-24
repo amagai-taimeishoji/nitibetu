1aa03832930d2035d555eb1c4e37528452dae3c0
amagai-taimeishoji <amagai.taimeishoji@gmail.com>
2025-09-24 21:52:44 +0900

    app.js を更新

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const YEAR = 2025;
 const MONTH = 10;
 const DAY_MIN = 1;
 const DAY_MAX = 30;
-const LOADING_DURATION_MS = 30000; // 15秒でMAX
+const LOADING_DURATION_MS = 30000; // 30秒でMAX
 // -------------------------------------------------------
 
 /* globals */
